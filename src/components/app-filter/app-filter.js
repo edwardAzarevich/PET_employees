44b653a4ea85adf4d9e8fd1f607a17833ab8f4f7
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2023-04-03 08:34:40 +0300

    add work sass/scss

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -5,19 +5,21 @@ import "./app-filter.css";
 
 const AppFilter = (props) => {
     const buttonData = [
-        { name: 'all', label: 'Все сотрудники' },
-        { name: 'rise', label: 'На повышение' },
-        { name: 'moreThen1000', label: 'З/П больше 1000$' }
+        { name: 'all', label: 'Все сотрудники', colorup: false },
+        { name: 'rise', label: 'На повышение', colorup: false },
+        { name: 'moreThen1000', label: 'З/П больше 1000$', colorup: true }
     ];
 
-    const buttons = buttonData.map(({ name, label }) => {
+    const buttons = buttonData.map(({ name, label, colorup }) => {
         const acrive = props.filter === name;
         const clazz = acrive ? 'btn-light' : 'btn-outline-ligh';
+        const style = colorup ? { color: 'green' } : null;
         return (
             <button type="button"
                 className={`btn ${clazz}`}
                 key={name}
-                onClick={() => props.onFilterSelect(name)}>
+                onClick={() => props.onFilterSelect(name)}
+                style={style}>
                 {label}
             </button>
         )
